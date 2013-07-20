2dd6546e2dcafc658e166afa1f57953d238e20c7
Frédéric G. MARAND <fgm@osinet.fr>
2013-07-20 06:49:52 -0700

    WIP public API.

---
+++ README.md
@@ -0,0 +1,4 @@
+statsd_php_client
+=================
+
+A configurable PHP client for StatsD
--- src/StatsdPhpClient/Client.php
+++ src/StatsdPhpClient/Client.php
@@ -15,12 +15,99 @@ class Client {
   protected $config;
 
   /**
+   * @var boolean
+   *   Is the script shutdown handler enabled ?
+   */
+  protected $isShutdownEnabled;
+
+  /**
+   * @var array
+   *   A metric-indexed hash of [value, sample_rate].
+   */
+  protected $queue;
+
+  /**
+   * Constructor. 
+   */
+  public function __construct(Config $config) {
+    $this->config = $config;
+    $this->isShutdownEnabled = false;
+  }
+
+  /**
    * @return \OSInet\StatsdPhpClient\Config
    */
   public function getConfig() {
     return $this->config;
   }
 
+  /**
+   * Enable a shutdown handler ensuring data flush on page shutdown.
+   */
+  protected function registerShutdown() {
+    $that = $this;
+    register_shutdown_function(function () use (Client $that) {
+      $that->doSend($that->queue);
+    });
+    $this->isShutdownEnabled = true;
+  }
+
+  /**
+   *  Deferred send.
+   *
+   * - passed data will be sent on next flush, which can be on page
+   *   shutdown.
+   *
+   * @param array $data
+   * @param float $sample_rate
+   *
+   * @return void
+   */
+  public function sendDeferred($data, $sample_rate = 1.0) {
+    // Deduplicate metrics with currently queued ones.
+    foreach ($data as $metric => $value) {
+      $this->queue[$metric] = array($value, $sample_rate);
+    }
+  }
+
+  /**
+   * Flush send: send data immediately, along with existing queued data.
+   * 
+   * On empty flush send: just send existing queued data.
+   *
+   * @var array $data
+   * @var float $sample_rate
+   * 
+   * @return void
+   */
+  public function sendFlush($data = array(), $sample_rate = 1.0) {
+    if (!empty($data)) {
+      $this->sendDeferred($data, $sample_rate);
+    }
+    $this->doSend($this->queue);
+  }
+
+  /**
+   * Immediate send.
+   *
+   * - passed data are sent immediately
+   * - queued data are not modified.
+   *
+   * @param array $data
+   * @param float $sample_rate
+   *
+   * @return void
+   */
+  public function sendImmediate($data, $sample_rate) {
+    $queue = array();
+    foreach ($data as $metric -> $value) {
+      $queue[$metric] = array($value, $sample_rate);
+    }
+    $this->doSend($queue);
+  }
+
+  function __wip____________________________________________________()(}
+  
     /**
      * Sets one or more timing values
      *
@@ -100,39 +187,59 @@ class Client {
         StatsD::send($data, $sampleRate);
     }
 
-    /*
-     * Squirt the metrics over UDP
-     **/
-    public static function send($data, $sampleRate = 1) {
-        $config = Config::getInstance();
-        if (! $config->isEnabled("statsd")) { return; }
-
-        // sampling
-        $sampledData = array();
-
-        if ($sampleRate < 1) {
-            foreach ($data as $stat => $value) {
-                if ((mt_rand() / mt_getrandmax()) <= $sampleRate) {
-                    $sampledData[$stat] = "$value|@$sampleRate";
-                }
-            }
-        } else {
-            $sampledData = $data;
+  /**
+   * Sample a data set to only send a sampling of the original data.
+   */
+  protected function sampleData($data, $sample_rate) {
+    $sampledData = array();
+    if ($sample_rate < 1) {
+      foreach ($data as $stat => $value) {
+        if ((mt_rand() / mt_getrandmax()) <= $sample_rate) {
+          $sampledData[$stat] = "$value|@$sample_rate";
         }
-
-        if (empty($sampledData)) { return; }
-
-        // Wrap this in a try/catch - failures in any of this should be silently ignored
-        try {
-            $host = $config->getConfig("statsd.host");
-            $port = $config->getConfig("statsd.port");
-            $fp = fsockopen("udp://$host", $port, $errno, $errstr);
-            if (! $fp) { return; }
-            foreach ($sampledData as $stat => $value) {
-                fwrite($fp, "$stat:$value");
-            }
-            fclose($fp);
-        } catch (Exception $e) {
-        }
+      }
+    } 
+    else {
+      $sampledData = $data;
     }
+    
+    return $sampledData;
+  }
+
+  /*
+   * Squirt the metrics over UDP
+   * 
+   * @var array $queue
+   *   A metric-indexed hash of [data, sample_rate] arrays. 
+   */
+  protected static function doSend($queue) {
+    $config = $this->config;
+    if (!$config->isEnabled("statsd")) { 
+      return; 
+    }
+
+    // FIXME adapt code for the send queue instead of the origina format.
+    
+    // Sampling
+    $sampledData = $this->sampleData($data, $sample_rate);
+    if (empty($sampledData)) {
+      return; 
+    }
+
+    // Any exception in any of this should be silently ignored.
+    try {
+      $host = $config->getConfig("statsd.host");
+      $port = $config->getConfig("statsd.port");
+      $fp = fsockopen("udp://$host", $port, $errno, $errstr);
+      if (!$fp) { 
+        return; 
+      }
+      foreach ($sampledData as $stat => $value) {
+        fwrite($fp, "$stat:$value");
+      }
+      fclose($fp);
+    } 
+    catch (Exception $e) {
+    }
+  }
 }
